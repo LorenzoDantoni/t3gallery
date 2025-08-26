9a5cf9015a495fbe6e7573c5189045beb197d711
LorenzoDantoni <lorenzo.dantoni@studenti.unipd.it>
2025-08-26 11:50:59 +0200

    Linked database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,34 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mocksUrls = [
-  "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j3i21glXBSONfg9C1YyGrcaLzVxnvT0suAXMoP",
-  "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j3Lirj1CuUqdlHENJ86h70w2ts4xBrzua1We9o",
-  "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j36Q5ZOUhXaIjgschtQV2MFdwYRT78KbfCr1eJ",
-  "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j354GjlvQHoVNzKQ9pqh7mvPSdOwA2DuTgn1J3",
-];
+// const mocksUrls = [
+//   "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j3i21glXBSONfg9C1YyGrcaLzVxnvT0suAXMoP",
+//   "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j3Lirj1CuUqdlHENJ86h70w2ts4xBrzua1We9o",
+//   "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j36Q5ZOUhXaIjgschtQV2MFdwYRT78KbfCr1eJ",
+//   "https://ftluvimrni.ufs.sh/f/3zIc3cRVu8j36Q5ZOUhXaIjgschtQV2MFdwYRT78KbfCr1eJ",
+// ];
 
-const mockImages = mocksUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+// const mockImages = mocksUrls.map((url, index) => ({
+//   id: index + 1,
+//   url,
+// }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  console.log(images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post, index) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} alt="image" />
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-      Hello (gallery in progress)
     </main>
   );
 }
